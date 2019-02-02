97857cae586aa08e9753d2317dbdc648d7f1f4a7
Nikhil Gupte <nikhilgupte@gmail.com>
2019-02-02 10:56:57 -0500

    fixed typo

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -5,6 +5,6 @@ export const router = new Router();
 router.get('/', async (ctx) => {
   ctx.body = {
     status: 'success',
-    message: 'hello, hello, I don\'t know why you day goodbye, I say hello...'
+    message: 'hello, hello, I don\'t know why you say goodbye, I say hello...'
   };
 })
